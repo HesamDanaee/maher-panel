c39baf271497ebb70eec76c25d31079d0127d0d9
Hesam Danaee <hesamdanaey@outlook.com>
2024-03-12 01:30:20 +0330

    change file structure & add swr, jest & react testing library packages

--- src/components/shadcn/form.tsx
+++ src/components/shadcn/form.tsx
@@ -11,7 +11,7 @@ import {
 } from "react-hook-form";
 
 import { cn } from "@/src/lib/utils";
-import { Label } from "@/src/components/label";
+import { Label } from "@/src/components/shadcn/label";
 
 const Form = FormProvider;
 
