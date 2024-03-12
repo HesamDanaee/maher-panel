9238326f9c5597aff922472c56fbff798d0440fd
Hesam Danaee <hesamdanaey@outlook.com>
2024-03-12 03:41:37 +0330

    Add apis

---
+++ src/constants/apis.ts
@@ -0,0 +1,16 @@
+const APIS = (() => {
+  const base = process.env.BASE_URL;
+  return {
+    base,
+    register: base + "/register",
+    login: base + "/auth/login",
+    isActive: base + "/panel/isActive",
+    sendSMS: base + "/panel/sendSms",
+    verify: base + "/panel/verify",
+    completeUser: base + "/panel/completeUser",
+    uploadScv: base + "/panel/goods/upload-csv",
+    getAllGoods: base + "/panel/goods/getAll",
+    goods: base + "/panel/goods/add",
+    addGoodsByFile: base + "/panel/goods/addByFile",
+  };
+})();
