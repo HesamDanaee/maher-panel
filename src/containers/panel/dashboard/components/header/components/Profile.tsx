20629f8377595b07a34fcd0191022034660acbd6
Hesam Danaee <hesamdanaey@outlook.com>
2024-03-20 17:48:36 +0330

    Add icons to profile modal and change some styles

--- src/containers/panel/dashboard/components/header/components/Profile.tsx
+++ src/containers/panel/dashboard/components/header/components/Profile.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import Flex from "@/src/components/common/Flex";
 import Typography from "@/src/components/common/Typography";
 import {
@@ -7,12 +5,13 @@ import {
   AvatarImage,
   AvatarFallback,
 } from "@/src/components/shadcn/avatar";
-import { Button } from "@/src/components/shadcn/button";
+
 import {
   Popover,
   PopoverContent,
   PopoverTrigger,
 } from "@/src/components/shadcn/popover";
+import { PiGearFine, PiUser } from "react-icons/pi";
 
 export default function Profile() {
   return (
@@ -31,28 +30,36 @@ export default function Profile() {
       <PopoverContent className="p-0" align="start">
         {/* Header */}
         <Flex className="flex-col border-b-[1px] border-secondary py-2 px-4">
-          <Typography variant="p">حسام دانائی</Typography>
-          <Typography variant="p" className="text-ghost">
+          <Typography variant="p" className="font-light">
+            حسام دانائی
+          </Typography>
+          <Typography variant="p" className="text-ghost font-extralight">
             HesamDanaey@outlook.com
           </Typography>
         </Flex>
 
         {/* Body */}
         <Flex className="flex-col py-2 px-1">
-          <Flex className="py-1 hover:cursor-pointer hover:bg-muted px-2 rounded-sm bgTransition">
-            <Typography variant="p">پروفایل</Typography>
+          <Flex className="justify-between items-center py-1 hover:cursor-pointer hover:bg-muted px-2 rounded-sm bgTransition">
+            <Typography variant="p" className="text-foreground font-extralight">
+              پروفایل
+            </Typography>
+            <PiUser className="w-5 h-5" />
           </Flex>
-          <Flex className="py-1 hover:cursor-pointer hover:bg-muted px-2 rounded-sm bgTransition">
-            <Typography variant="p" className="text-foreground">
+          <Flex className="justify-between items-center py-1 hover:cursor-pointer hover:bg-muted px-2 rounded-sm bgTransition group">
+            <Typography variant="p" className="text-foreground font-extralight">
               تنظیمات
             </Typography>
+            <PiGearFine className="w-5 h-5 group-hover:rotate-45 transition duration-100 ease-out" />
           </Flex>
         </Flex>
 
         {/* Footer */}
         <Flex className="border-t-[1px] border-secondary py-2 px-1">
           <Flex className="py-1 hover:cursor-pointer hover:bg-muted px-2 rounded-sm bgTransition">
-            <Typography variant="p">خروج از حساب</Typography>
+            <Typography variant="p" className="text-foreground font-extralight">
+              خروج از حساب
+            </Typography>
           </Flex>
         </Flex>
       </PopoverContent>
