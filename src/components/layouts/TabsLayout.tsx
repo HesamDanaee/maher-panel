be1476cdb12180343fd7caef69e373608c6cc5f1
Hesam Danaee <hesamdanaey@outlook.com>
2024-03-29 03:10:37 +0330

    Update the style of TabsLayout for better support in smaller screens

--- src/components/layouts/TabsLayout.tsx
+++ src/components/layouts/TabsLayout.tsx
@@ -8,7 +8,7 @@ interface TabsLayoutProps {
 export default function TabsLayout({ children }: TabsLayoutProps) {
   return (
     <Grid className="w-full h-full grid-cols-12 grid-rows-8 gap-0">
-      <GridCol className="flex flex-col gap-y-3 col-start-3 max-xl:col-start-2 max-sm:col-start-1 col-end-11 max-xl:col-end-12 max-sm:col-end-13 row-start-3 max-sm:row-start-2 row-end-9 max-sm:px-3">
+      <GridCol className="flex flex-col gap-y-3 col-start-3 max-2xl:col-start-2 max-sm:col-start-1 col-end-11 max-2xl:col-end-12 max-sm:col-end-13 row-start-3 max-sm:row-start-2 row-end-9 max-sm:px-3">
         {children}
       </GridCol>
     </Grid>
