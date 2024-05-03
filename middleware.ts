b4004bc40ac133896e2ca7ef256b9ed5c0ce20dc
Hesam <hesamdanaey@outlook.com>
2024-05-03 15:35:02 +0330

    refactor:change pathes

--- middleware.ts
+++ middleware.ts
@@ -1,24 +1,15 @@
-import { type NextRequest, NextResponse } from "next/server";
+import { type NextRequest } from "next/server";
 
-export function middleware(request: NextRequest) {
+export async function middleware(request: NextRequest) {
   const token = request.cookies.get("token")?.value;
 
-  // If user has a valid token and request is coming from the login page redirect him to the panel
-  if (token && request.nextUrl.pathname.startsWith("/auth/login")) {
-    return Response.redirect(new URL("/panel/dashboard", request.url));
+  if (!token) {
+    return Response.redirect(new URL("/auth/login", request.url));
+  } else {
+    return Response.redirect(
+      new URL("/panel/dashboard/customers", request.url)
+    );
   }
-
-  // if (token && !request.nextUrl.pathname.startsWith("/dashboard")) {
-  //   return Response.redirect(new URL("/dashboard/", request.url));
-  // }
-
-  if (!token && !request.nextUrl.pathname.startsWith("/login")) {
-    return Response.redirect(new URL("/auth/signup", request.url));
-  }
-
-  return NextResponse.redirect(
-    new URL("/panel/dashboard/customers", request.url)
-  );
 }
 
 export const config = {
