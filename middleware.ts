3ea7e3a06abbc270f7df37a1aced911f3549b448
Hesam <hesamdanaey@outlook.com>
2024-06-05 02:12:15 +0330

    fix: update matcher and middleware

--- middleware.ts
+++ middleware.ts
@@ -3,15 +3,9 @@ import { type NextRequest } from "next/server";
 export async function middleware(request: NextRequest) {
   const token = request.cookies.get("token")?.value;
 
-  if (!token) {
-    return Response.redirect(new URL("/auth/login", request.url));
-  } else {
-    return Response.redirect(
-      new URL("/panel/dashboard/customers", request.url)
-    );
-  }
+  if (!token) return Response.redirect(new URL("/auth/login", request.url));
 }
 
 export const config = {
-  matcher: "/",
+  matcher: ["/panel/dashboard/:path*", "/register/:path*"],
 };
