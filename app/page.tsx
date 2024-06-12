4a48f705983ac75f8f8ad6b373e08e40c745350a
Hesam <hesamdanaey@outlook.com>
2024-06-12 23:47:21 +0330

    add redirect to main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,7 @@
-
+import { redirect } from "next/navigation";
 
 export default function Home() {
-
-
+  return redirect("/auth/login");
 
   return (
     <main className="flex bg-background min-h-screen flex-col items-center justify-between p-24"></main>
