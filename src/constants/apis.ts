7b3ad0b29e64ec8dd135368733c3abc8c0bf25b0
Hesam <hesamdanaey@outlook.com>
2024-06-28 11:42:45 -0700

    feat:add new apis

--- src/constants/apis.ts
+++ src/constants/apis.ts
@@ -1,23 +1,40 @@
 const APIS = (() => {
   const base = process.env.BASE_URL;
   const clientBaseURL = process.env.NEXT_PUBLIC_BASE_URL;
+  const authBaseURL = clientBaseURL + "/auth/";
+  const panelBaseURL = clientBaseURL + "/panel/";
+  const goodsBaseURL = panelBaseURL + "/goods/";
+  const customerBaseURL = clientBaseURL + "/customers/";
 
   return {
     base,
-    register: clientBaseURL + "/register",
-    login: clientBaseURL + "/auth/login",
-    reset: clientBaseURL + "/auth/forgotPass",
-    resetCode: clientBaseURL + "/resetPassLink",
-    isActive: clientBaseURL + "/panel/isActive",
-    sendSMS: clientBaseURL + "/panel/sendSms",
-    verify: clientBaseURL + "/panel/verify",
-    completeUser: base + "/panel/completeUser",
-    addCustomer: clientBaseURL + "/panel/customers/add",
-    updateCustomer: clientBaseURL + "/panel/customers/update",
-    uploadScv: base + "/panel/goods/upload-csv",
-    getAllGoods: base + "/panel/goods/getAll",
-    goods: base + "/panel/goods/add",
-    addGoodsByFile: base + "/panel/goods/addByFile",
+    register: clientBaseURL + "register",
+    resetCode: clientBaseURL + "resetPassLink",
+    panel: {
+      registerUser: panelBaseURL + "registerUser",
+      profile: panelBaseURL + "getProfile",
+      isActive: panelBaseURL + "isActive",
+      isAdmin: panelBaseURL + "isAdmin",
+      sendSMS: panelBaseURL + "sendSms",
+      verify: panelBaseURL + "verify",
+      completeUser: panelBaseURL + "completeUser",
+      addCustomer: panelBaseURL + "customers/add",
+      goods: {
+        csv: goodsBaseURL + "upload-csv",
+        goodsList: goodsBaseURL + "getAll",
+        add: goodsBaseURL + "add",
+        addByFile: goodsBaseURL + "addByFile",
+      },
+      customer: {
+        add: customerBaseURL + "add",
+        getCustomerList: customerBaseURL + "getList",
+        update: customerBaseURL + "update",
+      },
+    },
+    auth: {
+      login: authBaseURL + "login",
+      reset: authBaseURL + "forgotPass",
+    },
   };
 })();
 
